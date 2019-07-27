43f973ebcb10f5ed12929f3ccb60f9501a062ee1
Emanuel Fernandes <efernandes@tektorque.com>
2019-07-27 16:51:43 +0100

    remove next api files

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-export default function handle(req, res) {
-  // tslint:disable-next-line: no-console
-  console.log(req.body) // The request body
-  // tslint:disable-next-line: no-console
-  console.log(req.query) // The url querystring
-  // tslint:disable-next-line: no-console
-  console.log(req.cookies) // The passed cookies
-  res.end('Hello World')
-}
