90d81f2ef9788df7445d2de4412a382826f3d607
Emanuel Fernandes <efernandes@tektorque.com>
2019-08-12 17:43:21 +0100

    fix prod build

--- next.config.js
+++ next.config.js
@@ -11,12 +11,32 @@ module.exports = () => {
     )
 
     // fix: prevents error when .less files are required by node
-    if (typeof require !== 'undefined') {
-        require.extensions['.less'] = file => { }
-    }
+    // if (typeof require !== 'undefined') {
+    //     require.extensions['.less'] = file => { }
+    // }
 
     return withLess({
-        webpack: (config, options) => {
+        webpack: (config, { isServer }) => {
+            if (isServer) {
+                const antStyles = /antd\/.*?\/style.*?/
+                const origExternals = [...config.externals]
+                config.externals = [
+                    (context, request, callback) => {
+                        if (request.match(antStyles)) return callback()
+                        if (typeof origExternals[0] === 'function') {
+                            origExternals[0](context, request, callback)
+                        } else {
+                            callback()
+                        }
+                    },
+                    ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+                ]
+
+                config.module.rules.unshift({
+                    test: antStyles,
+                    use: 'null-loader',
+                })
+            }
             return config
         },
         lessLoaderOptions: {
